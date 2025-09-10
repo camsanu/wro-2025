c460dcc7e571645c14a88e4715f147035663f3cc
cam <camsandoval008@gmail.com>
2025-09-10 13:12:39 -0700

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -3,29 +3,36 @@
 #define motFwd  7
 #define motRev  6
 
+// rgb red (238, 39, 55)
+// rgb green (68, 214, 44)
+// rgb mag (255, 0, 255)
+
 Servo myservo;
 
 const int trigPin = 12, echoPin_f = 11, echoPin_r = 10, echoPin_l = 9; // ultrasonic pins
 const int buttonPin = 8;
+const int sMin = 50, sMax = 130, sCenter = 90; // servo limits
+const int threshold = 100; // distance threshold
 
 int lDistance, rDistance, fDistance;
-int corners = 0; // amount of corners turned so far
-int Gp = 1; // proportional gain
-int Gd = 1; // derivative gain
-int Gi = 1; // integral gain
-static int prevErr = 0; // store previous for derivative
-static int integ = 0; // store integral
+int corners = 0; // corners turned so far
+float Gp = 0.6; // proportional gain
+float Gd = 1; // derivative gain
+float Gi = 0.02; // integral gain
+static float prevError = 0;
+static float integ = 0; // store integral
 
 int numReadings = 3; // samples
 
 bool pushEd = false; // button state
 int pushEs = 0; // button state counter
+bool lastState = HIGH; // save button state
 bool red = false; // obj detection
 bool green = false; 
 
 long getDistance(int echoPin){ // distance capture function
     long duration;
-    int sum = 0;
+    long sum = 0;
 
     for(int i = 0; i < numReadings; i++){
       digitalWrite(trigPin, LOW);
@@ -34,7 +41,7 @@ long getDistance(int echoPin){ // distance capture function
       delay(10);
       digitalWrite(trigPin, LOW);
 
-      duration = pulseIn(echoPin, HIGH);
+      duration = pulseIn(echoPin, HIGH, 20000);
       sum += (duration * 0.034 / 2);
     }
   return sum / numReadings;
@@ -73,9 +80,8 @@ void dead(){
 
 void endP() { // route natural end
   dead();
+  myservo.write(sCenter);
   Serial.println("Over");
-  while (true) {
-  }
 }
 
 void mCalib(){ // motor calibration
@@ -96,27 +102,47 @@ void sCalib(){ // servo calibration
   Serial.println("Servo check");
   myservo.write(180);
   delay(100);
-  myservo.write(90);
+  myservo.write(sCenter);
   Serial.println("End of Servo calibration"); 
 }
 
 void lTurn(){ // turn left corner
-  myservo.write(0);
-  delay(800);
-  if(lDistance < 100){
-    myservo.write(90);
-    corners++;
-    Serial.println("Turned Left");
+  static unsigned long turnStart = 0; // when the turn started
+  static bool turning = false;
+  if(!turning){
+    myservo.write(0);
+    turnStart = millis();
+    turning = true;
+    Serial.println("Turning Left");
+  }
+
+  if(millis() - turnStart >= 800){
+    if(lDistance < threshold){
+      myservo.write(sCenter);
+      corners++;
+      Serial.println("Turned Left");
+    }
+    turning = false;
   }
 }
 
 void rTurn(){ // turn right corner
-  myservo.write(180);
-  delay(800);
-  if(rDistance < 100){
-    myservo.write(90);
-    corners++;
-    Serial.println("Turned Right");
+  static unsigned long turnStart = 0; // when the turn started
+  static bool turning = false;
+  if(!turning){
+    myservo.write(180);
+    turnStart = millis();
+    turning = true;
+    Serial.println("Turning Right");
+  }
+
+  if(millis() - turnStart >= 800){
+    if(rDistance < threshold){
+      myservo.write(sCenter);
+      corners++;
+      Serial.println("Turned Right");
+    }
+    turning = false;
   }
 }
 
@@ -143,33 +169,37 @@ void setup() {
 // main loop
 
 void loop() {
-  if(digitalRead(buttonPin) == LOW){ // save button state
-    pushEd = true;
+  bool buttonState = digitalRead(buttonPin);
+  if(lastState == HIGH && buttonState == LOW){ // save button state
     pushEs++;
-    Serial.println("Started");
-    if(pushEs == 2){ // kill switch
+    if(pushEs == 1){
+      pushEd = true;
+      Serial.println("Started");
+    } else if(pushEs > 1){ // kill switch
       pushEd = false;
+      Serial.println("Killed");
     }
+    delay(50); // debounce
   }
+  lastState = buttonState;
 
   if(pushEd == true){
     fwd();
-    Serial.println("Forward");
     saveDistances();
     printDistances();
 
     if(lDistance == rDistance){
-      myservo.write(90); // straight
+      myservo.write(sCenter);
     }
   
-    if(lDistance != rDistance && fDistance>100 && (red && green == false)){ // overshoot
-      int err = lDistance - rDistance;
-      int deriv = err - prevErr;
-      int servoAngle = 90 + (Gp * err) + (Gd * deriv) + (Gi * integ); // proportional + derivative control
-      prevErr = err;
-      integ += err;
-      servoAngle = constrain(servoAngle, 50, 130); // limit to safe range
-      integ = constrain(integ, -100, 100); // limit integral windup
+    if(lDistance != rDistance && fDistance>threshold && (!red && !green)){ // overshoot
+      float error = lDistance - rDistance;
+      float deriv = error - prevError;
+      float servoAngle = sCenter + (Gp * error) + (Gd * deriv) + (Gi * integ); // pid control
+      prevError = error;
+      integ += error;
+      servoAngle = constrain(servoAngle, sMin, sMax); // limit to safe range
+      integ = constrain(integ, -threshold, threshold); // limit integral windup
       if(lDistance > rDistance){
         myservo.write(servoAngle);
         Serial.println("Left Overshoot");
@@ -180,24 +210,33 @@ void loop() {
       }
     }
 
-    if(fDistance < 100 && (red && green == false)){
+    if(fDistance < threshold && (!red && !green)){
       if(lDistance > rDistance){ // left turn
         lTurn();
+        integ = 0;
       }
       else if(rDistance > lDistance){ // right turn
         rTurn();
+        integ = 0;
       }
     } 
   
-    if(corners==4 && (red && green == false)){
-      delay(800);
-      endP();
+    if(corners==4 && (!red && !green)){
+      static unsigned long shutStart = 0; // when the shut down started
+      static bool shutting = false;
+      if(!shutting){
+        shutStart = millis();
+        shutting = true;
+        Serial.println("Shutting down");
+      }
+      if(millis() - shutStart >= 800){
+        Serial.println("Shut down complete");
+        shutting = false;
+        endP();
+      }
     }
   } 
   if(pushEd == false && pushEs > 1){
-    dead();
-    myservo.write(90);
-    Serial.println("Killed");
-    for(;;){}
+    endP();
   }
 }
