48f1f92af6ef9ff64969565b7260f95d2fc65920
cam <camsandoval008@gmail.com>
2025-09-18 07:28:33 -0700

    Update motorTest.ino

--- src/tests/motorTest/motorTest.ino
+++ src/tests/motorTest/motorTest.ino
@@ -1,22 +1,34 @@
-// Pines del puente H
-#define ENA 3   // PWM para velocidad
-#define IN1 4   // Dirección
-#define IN2 2   // Dirección
+
+#define IN2Fwd 2
+#define IN1Rev 4
+#define ENA 3
 
 void setup() {
-  pinMode(ENA, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN1, OUTPUT);
+  pinMode(ENA, OUTPUT);
 
-  Serial.begin(9600);   // Inicia comunicación serial
-  Serial.println("Iniciando control de motor...");
+  Serial.begin(9600);
+  Serial.println("Started");
 }
 
 void loop() {
-  // ---- Giro hacia adelante ----
-  Serial.println("Motor: Adelante al 100%");
+  Serial.println("Forward: 100%");
   digitalWrite(IN2, HIGH);
   digitalWrite(IN1, LOW);
-  analogWrite(ENA, 255);   // Velocidad máxima
+  analogWrite(ENA, 255);
+  delay(2000);
+  Serial.println("Stopped");
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN1, LOW);
+  delay(2000);
+  Serial.println("Reverse: 100%");
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN1, LOW);
+  analogWrite(ENA, 255);
+  delay(2000);
+  Serial.println("Stopped");
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN1, HIGH);
   delay(2000);
 }
