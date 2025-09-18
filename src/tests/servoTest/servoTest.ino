46d735b9f3a376c05933aea04e669c2fc2384289
cam <camsandoval008@gmail.com>
2025-09-18 07:39:48 -0700

    Update servoTest.ino

--- src/tests/servoTest/servoTest.ino
+++ src/tests/servoTest/servoTest.ino
@@ -3,35 +3,30 @@
 Servo myservo;
 
 const int buttonPin = 8;
+const int sMin = 80, sMax = 180, sCenter = 130; // servo limits
+
 bool pushEd = false; // button state
 int pushEs = 0; // button state counter
 
 void setup(){
   pinMode(buttonPin, INPUT_PULLUP);
-  myservo.attach(13);
+  myservo.attach(6);
   Serial.begin(9600);
 }
 
 void loop(){
   if(digitalRead(buttonPin) == LOW){ // save button state
     pushEd = true;
-    pushEs++;
-    if(pushEs == 2){ // kill switch
-      pushEd = false;
-    }
   }
   if(pushEd == true){
     Serial.println("Right");
-    myservo.write(0);
+    myservo.write(sMin);
     delay(1000);
     Serial.println("Center");
-    myservo.write(90);
+    myservo.write(sCenter);
     delay(1000);
     Serial.println("Left");
-    myservo.write(180);
+    myservo.write(sMax);
     delay(1000);
-  }
-  if(pushEd == false && pushEs > 1){
-    Serial.println("Killed");
   }
 }
