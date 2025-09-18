ffa01249bf1366778cb0d96e7dfa9e6c84564474
cam <camsandoval008@gmail.com>
2025-09-18 07:30:23 -0700

    Update sensorTest.ino

--- src/tests/sensorTest/sensorTest.ino
+++ src/tests/sensorTest/sensorTest.ino
@@ -2,13 +2,12 @@ const int trigPin = 8;
 const int echoPin_1 = 6; // right sensor echo
 const int echoPin_2 = 7; // left sensor echo
 const int echoPin_3 = 9; // front sensor echo
-// Pin del botón
+
 const int buttonPin = 11;
 
 int fDistance; // front
 int lDistance; // left 
 int rDistance; // right
-int turns = 0; // number of corners turned
 
 int numReadings = 3; // number of samples before printing
 bool pushEd = false;
@@ -21,12 +20,10 @@ void setup() {
   pinMode(echoPin_1, INPUT);
   pinMode(echoPin_2, INPUT);  
   pinMode(echoPin_3, INPUT);
-
-  // Configurar el botón con resistencia pull-up interna
   pinMode(buttonPin, INPUT_PULLUP);
 }
 
-long getDistance(int echoPin){ // captures distance for an n number of times and averages them for more consistent output
+long getDistance(int echoPin){
     long duration;
     int sum = 0;
 
@@ -49,18 +46,15 @@ void loop() {
     pushEd = true;
   }
   if (pushEd == true) {
-    // Medir distancias con cada sensor
-    fDistance = getDistance(echoPin_3); // collect distances
+    fDistance = getDistance(echoPin_3);
     lDistance = getDistance(echoPin_2); 
     rDistance = getDistance(echoPin_1);
   
-    Serial.print("FLR: "); // print them out to serial monitor
+    Serial.print("FLR: ");
     Serial.print(fDistance);
     Serial.print(" ");
     Serial.print(lDistance);
     Serial.print(" ");
     Serial.println(rDistance);
-
-    Serial.println("Forward");
   }
 }
